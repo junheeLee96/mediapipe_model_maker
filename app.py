3c072b429583fa682ba9c9b994cef813cb4d3a48
junheeLee96 <dlwnsgml203@naver.com>
2024-05-09 11:19:07 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import mediapipe as mp
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+print('hello world')
